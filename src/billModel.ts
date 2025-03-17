3bfb21b827c655526c0c113d1622119b97d05f67
13felixblakemore <13felix.blakemore@gmail.com>
2025-03-17 16:54:40 +0000

    changed bill schema

---
+++ src/billModel.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const billSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    Item:{
+        type: String,
+        required: true,
+    },
+    Payee: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    Amount: {
+        type: Number,
+        required: true
+    }, 
+    Payors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+    Status: {
+        type: String, 
+        enum: ['Unpaid', 'Paid', 'Confirmed'],
+        required: true
+    }, 
+    Deadline: {
+        type: Date,
+        required: false 
+    },
+    Recurring: {
+        type: String,
+        enum: ['Weekly', 'Biweekly', 'Monthly', 'None'], 
+        required: false
+    },
+    Flag: {
+        type: Boolean,
+        required: false
+    }
+  });
+
+const BillModel = mongoose.model('Bill', billSchema);
+export default BillModel;
